69af80765e2635876b53e9914a1f6ea1ceab48e6
Alabi, Hilary {MDPA~Welwyn} <hilary.alabi@roche.com>
2019-12-18 13:58:22 +0100

    commit

--- Linear_regression.R
+++ Linear_regression.R
@@ -1,1 +1,14 @@
+# https://www.datacamp.com/community/tutorials/linear-regression-R
+# https://www.machinelearningplus.com/machine-learning/complete-introduction-linear-regression-r/
 # Linear regression 
+
+library(readxl) # To read in excel files
+
+ageandheight <- read_excel("~/ageandheight.xls")
+
+# lm[target variable] ~ [predictor variable], data = [data source]
+
+lmheight <- lm(height ~ age, data = ageandheight)
+summary(lmheight)
+
+# the model predicts (on average) that its height in centimeters is around 64.92 + (0.635 * 20.5) = 77.93 cm.
--- regularization.R
+++ regularization.R
@@ -1,1 +1,2 @@
+# https://www.datacamp.com/community/tutorials/tutorial-ridge-lasso-elastic-net
 # Regularization: Ridge, Lasso and Elastic Net
